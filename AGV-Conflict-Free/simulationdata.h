515669a4414c7726ccdd7be1962b43c38939f183
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2025-01-25 13:53:35 +0330

    some simulation data added

--- AGV-Conflict-Free/simulationdata.h
+++ AGV-Conflict-Free/simulationdata.h
@@ -3,26 +3,46 @@
 // #include <iostream>
 #include <QVector>
 #include <tuple>
-typedef struct {
-    QVector<std::tuple<int,char>> jobs; //(1,'L') means first job is loading
-} QuayCrane;
+//set of QCs should be called QCBuffer,
+//set of AGVs shold be called AGVBuffer
 
-struct {
+// Problem specific simulation data
+struct QuayCrane {
+    int m; // the index
+    QVector<int> jobs; // means first job is loading
+};
+
+// int q;  //maximumNumberOfContainers conducted by each AGV
+struct container {
+    QuayCrane m;
+    int i; // which is jobs[i]
+    bool  Loading;
+};
+
+struct AllContainers{
+    QVector<QuayCrane> L;   //Loading Containers
+    QVector<QuayCrane> D;   //unloading containers
+};
+
+struct AGV {
     QVector<std::tuple<QuayCrane, int>> assignedContainers;
-    int TimeOfCurrentjob();
-    int q;  //maximumNumberOfContainers conducted by AGV
-} AGV;
+    int CompletionTimeOfCurrentjob(); //T(l)
+
+};
 
 QVector<QVector<int>> schemes;
 
-struct {
 
-} container;
+//Branch and bound simulation data
+typedef struct node* node_p;
+struct node {
+    node_p pred;
+    node_p child;
+    // node_p right_sibling;
+    // node_p left_sibling;
+    // long subtree;
 
-struct {
-
-
-} node;
+};
 
 
 #endif // SIMULATIONDATA_H
