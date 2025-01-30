5e587c8364d3e764ff69603e2d4a5c4aa2dc8b22
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2025-01-30 20:40:18 +0330

    upd

--- AGV-Conflict-Free/simulationdata.h
+++ AGV-Conflict-Free/simulationdata.h
@@ -11,12 +11,10 @@
 // Problem specific simulation data
 struct QuayCrane {
     int m; // the index
-    QQueue<int> jobs; // means first job is loading
+    QQueue<int> jobs;
 };
+enum action {a0 ,a1, a2, a3, a4};
 
-struct W{//set of actions on each container
-
-};
 
 // int q = T_UB / tMin;  //maximumNumberOfContainers conducted by each AGV
 struct container {
@@ -24,18 +22,15 @@ struct container {
     int i; // which is jobs[i]
     bool  isLoading;
     //according to QC m location and the block
-    QVector<std::tuple<container, double>> S;//list of container op time
-    QVector<std::map<double, QVector<W>>> R;// R(s(m,i), [routes])
-    QVector<QVector<W>> BuildR(container &c);
 };
 
-struct Block{
+typedef std::tuple<container,QuayCrane,action> W; //set of actions on each container
 
+
+struct Block{
     QVector<container> requestedContainers;  // this could be used for creating set \psi_2
     QVector<std::map<container,int>> AL;
     QVector<std::map<container,int>> AR;
-
-
 };
 
 
@@ -43,22 +38,36 @@ struct Block{
 struct AllContainers{
     QVector<container> L;       //Loading Containers
     QVector<container> D;       //unloading containers
-    double tMin;                 //minimum operation time for containers without considering conflict, used for computing AGV scheme
+    double tMin;                //minimum operation time for containers without considering conflict, used for computing AGV scheme
+
+    ////////////// possibly bad!
+    QVector<std::map<container, QVector<std::tuple<container, double>>>> S;//list of container op time
+    QVector<std::map<container, QVector<std::tuple<double, QVector<W>>>>> R;// R(s(m,i), [routes])
 };
+
+extern QVector<std::map<container, QVector<std::tuple<double, QVector<W>>>>> BuildR(container &c);
 
 struct AGV {
     int l; // the index
     QVector<std::tuple<QuayCrane, int>> assignedContainers;
     int CompletionTimeOfCurrentjob(); //T(l)
     int num; //number of assigned containers to an AGV
-
 };
 
-//port layout simulation data
-// should be more interactive and customizable
+///////////////////////////////////////////////////////
+//model specific parameters:
+
+//port layout
 int XR[23] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
 int YR[10] = {1,2,3,4,5,6,7,8,9,10};
 int YL[5] = {1,2,3,4,5};
 int YS[5] = {6,7,8,9,10};
+
+//QC vertical path
+typedef std::map<container, QVector<int>> O_Container;
+
+//sequence of QCS and ASCs
+typedef std::tuple<QuayCrane,container,QuayCrane,container> psi1;
+typedef std::tuple<QuayCrane,container,QuayCrane,container> psi2;
 
 #endif // SIMULATIONDATA_H
